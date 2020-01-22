44266e6754fde5065f2dec7c8035d65972ac3d70
Diego Bendersky <diego.bendersky@jampp.com>
2020-01-22 14:41:59 -0300

    fix setuptools_ext (create folder and set modules as list

--- pyrobuf/setuptools_ext.py
+++ pyrobuf/setuptools_ext.py
@@ -7,11 +7,24 @@ except NameError:
     # Python 3.x
     basestring = str
 
+import sys
+import os
+if sys.version_info.major == 3:
+    _FileExistsError = FileExistsError
+else:
+    _FileExistsError = OSError
+
 
 def add_pyrobuf_module(dist, pyrobuf_module):
     dir_name = "pyrobuf/_" + pyrobuf_module
-    compiler = Compiler(pyrobuf_module, out=dir_name,
-                        package="{}_{}".format(dist.get_name(), pyrobuf_module))
+    package = "{}_{}".format(dist.get_name(), pyrobuf_module)
+    try:
+        os.makedirs(os.path.join(dir_name, package))
+    except _FileExistsError:
+        pass
+
+    compiler = Compiler([pyrobuf_module], out=dir_name,
+                        package=package)
     compiler.extend(dist)
 
 
